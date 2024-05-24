e7776072742ebba853e8fd6a9bbef3482255b794
tratchapong <tratchapong@gmail.com>
2024-05-24 17:24:56 +0700

    AuthContext load token from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import useAuth from "./hooks/useAuth"
 import AppRouter from "./routes/AppRouter"
 
 
 function App() {
+  const {loading} = useAuth()
+
+  if(loading) {
+    return (
+      <h1 className="text-4xl text-center">Loading...</h1>
+    )
+  }
 
   return (
     <div className="min-h-screen max-w-3xl mx-auto shadow-lg">
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,16 +1,40 @@
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
+import axios from 'axios'
 
 const AuthContext = createContext()
 
 
 function AuthContextProvider(props) {
 	const [user, setUser] = useState(null)
+	const [loading, setLoading] = useState(true)
+
 	const logout = () => {
 		setUser(null)
 		localStorage.removeItem('token')
 	}
+	useEffect( ()=>{
+		const run = async () => {
+			try {
+				setLoading(true)
+				const token = localStorage.getItem('token')
+				if(!token) {
+					return
+				}
+				const rs = await axios.get('http://localhost:8889/auth/me', {
+					headers : { Authorization : `Bearer ${token}`}
+				})
+				setUser(rs.data.user)
+			} catch (err) {
+				console.log(err)
+			} finally {
+				setLoading(false)
+			}
+		}
+		run()
+	},[] ) 
+	
 	return (
-		<AuthContext.Provider value={ { user, setUser, logout} }>
+		<AuthContext.Provider value={ { user, setUser, logout, loading} }>
 			{props.children}
 		</AuthContext.Provider>
 	)
